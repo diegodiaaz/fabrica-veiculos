7fa42406408ff13ddd7c162f727bb5726b762fdd
vitorlpx <lopessccp.3k@gmail.com>
2023-06-20 16:11:31 -0300

    Versão final da r1 do projeto Fábrica de Veículos.

--- fabricaVeiculos/src/Veiculos/Teste.java
+++ fabricaVeiculos/src/Veiculos/Teste.java
@@ -7,7 +7,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-	Moto motinha = new Moto("2022", "Preta", "SKK-0912", "2", "Ninja", "Kawasaki", "600", "Flex", true);
+	//Moto motinha = new Moto("2022", "Preta", "SKK-0912", "2", "Ninja", "Kawasaki", "600", "Flex", true);
 //		motinha.ligar();
 //		motinha.acelerar();
 //		motinha.frear();
@@ -24,7 +24,7 @@ public class Teste {
 		carro.desligar();
 		carro.statusCarro();
 	
-	Caminhao caminhao = new Caminhao("2021", "Azul", "CSW-0999", "6", true, "15Kg", "10m", "Volvo", "Volvo VM", "Diesel");
+	//Caminhao caminhao = new Caminhao("2021", "Azul", "CSW-0999", "6", true, "15Kg", "10m", "Volvo", "Volvo VM", "Diesel");
 //		caminhao.ligar();
 //		caminhao.acelerar();
 //		caminhao.abrirPorta();
